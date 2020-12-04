c6297e1ebe4037666d0bd28dc31e3b4ed7cab1d2
dore51 <eitan.dor@gmail.com>
2020-12-04 17:53:44 +0200

    confict

--- src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
+++ src/main/java/bgu/spl/mics/application/passiveObjects/Ewoks.java
@@ -8,7 +8,7 @@ import java.util.List;
  * This class must be implemented as a thread-safe singleton.
  * You must not alter any of the given public methods of this class.
  * <p>
- * You can add ONLY private methods and fields to this class.
+ *
  */
 public class Ewoks {
 
