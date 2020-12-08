231aaa360a81a5112cde60283df45bb3fa4984ea
dore51 <eitan.dor@gmail.com>
2020-12-08 20:10:15 +0200

    MsgBus finished

--- src/main/java/bgu/spl/mics/MessageBusImpl.java
+++ src/main/java/bgu/spl/mics/MessageBusImpl.java
@@ -1,6 +1,5 @@
 package bgu.spl.mics;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Vector;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
@@ -13,14 +12,12 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 public class MessageBusImpl implements MessageBus {
 
-	// *check if maps need to be concurrent maps*
 	private final HashMap<Event, Future> eventToFutureMap; // Each event has a specific Future object related to him.
-
 	private final ConcurrentHashMap<MicroService, LinkedBlockingQueue<Message>> microServiceToMsgQueueMap; // Each Micro Service has a messages queue
 	private final ConcurrentHashMap<Class<? extends Event>, ConcurrentLinkedQueue<MicroService>> eventToMicroServicesQueueMap; // Each type of event has matching subscribers queue
-	private final ConcurrentHashMap<Class<? extends Broadcast>, Vector<MicroService>> broadcastToMicroServiceQueueMap; // Each type of broadcast has matching subscribers list
-	private final ConcurrentHashMap<MicroService, Vector<Class<? extends Event>>> microServiceToEvent; // given a micro-service, get all the types of events it registered to.
-	private final ConcurrentHashMap<MicroService, Vector<Class<? extends Broadcast>>> microServiceToBroadcast; // given a micro-service, get all the types of broadcasts it registered to.
+	private final ConcurrentHashMap<Class<? extends Broadcast>, ConcurrentLinkedQueue<MicroService>> broadcastToMicroServiceQueueMap; // Each type of broadcast has matching subscribers list
+	private final ConcurrentHashMap<MicroService, ConcurrentLinkedQueue<Class<? extends Event>>> microServiceToEvent; // given a micro-service, get all the types of events it registered to.
+	private final ConcurrentHashMap<MicroService, ConcurrentLinkedQueue<Class<? extends Broadcast>>> microServiceToBroadcast; // given a micro-service, get all the types of broadcasts it registered to.
 
 	private static class MessageBusHolder {
 		private static MessageBusImpl instance = new MessageBusImpl();
@@ -39,155 +36,89 @@ public class MessageBusImpl implements MessageBus {
 		microServiceToBroadcast = new ConcurrentHashMap<>();
 	}
 
-
-
 	@Override
 	public <T> void subscribeEvent(Class<? extends Event<T>> type, MicroService m) {
-		// check if m is registered
-		// check if already subscribed to that type of event
-		if (eventToMicroServicesQueueMap.get(type) == null) // first micro service to subscribe for this type of event
-			eventToMicroServicesQueueMap.put(type,new ConcurrentLinkedQueue<>()); // create new round robin queue for this type
-
-		// Maybe to synchronize eventToMicroServicesQueueMap.get(type) because what if add m
-		// in the middle of another ms is polled out of the queue in round robin
-		synchronized (eventToMicroServicesQueueMap.get(type)) { // round robin sync
-			eventToMicroServicesQueueMap.get(type).add(m); // add the micro service to the queue
-		}
-		microServiceToEvent.get(m).add(type); // add the type to the list of types which m subscribed to
-
+		eventToMicroServicesQueueMap.putIfAbsent(type,new ConcurrentLinkedQueue<>()); // if absent create new round robin queue for this type
+		eventToMicroServicesQueueMap.get(type).add(m); // add the micro service to the queue of micro-services subscribed to type event
+		microServiceToEvent.get(m).add(type); // add the event type to the list of types which m subscribed to
 	}
 
 	@Override
 	public void subscribeBroadcast(Class<? extends Broadcast> type, MicroService m) {
-		if (broadcastToMicroServiceQueueMap.get(type) == null) // first micro service to subscribe for this type of broadcast
-			broadcastToMicroServiceQueueMap.putIfAbsent(type,new Vector<>()); // create new list for this type
-		synchronized (broadcastToMicroServiceQueueMap.get(type)) {
-			microServiceToBroadcast.get(m).add(type); // add the type to the list of types which m subscribed to
-			broadcastToMicroServiceQueueMap.get(type).add(m); // add the micro service to the list
-		}
-
+		broadcastToMicroServiceQueueMap.putIfAbsent(type,new ConcurrentLinkedQueue<>()); // create new list for this type if absent
+		microServiceToBroadcast.get(m).add(type); // add the type to the list of types which m subscribed to
+		broadcastToMicroServiceQueueMap.get(type).add(m); // add the micro service to the list
     }
 
 	@Override
 	public <T> void complete(Event<T> e, T result) {
 		if (eventToFutureMap.containsKey(e)) {
-			eventToFutureMap.get(e).resolve(result);
+			eventToFutureMap.get(e).resolve(result); // getting future related to event e and resolve it to result
 			eventToFutureMap.remove(e);
 		}
-		else // DEBUG PURPOSES
-			System.out.println("didn't found matching future for event: " + e);
 	}
 
 	@Override
 	public void sendBroadcast(Broadcast b) {
-		Vector<MicroService> subscribers = broadcastToMicroServiceQueueMap.get(b.getClass());
-		if (subscribers != null) {
-			synchronized (broadcastToMicroServiceQueueMap.get(b.getClass())) {
-				for (Iterator<MicroService> iterator = broadcastToMicroServiceQueueMap.get(b.getClass()).iterator(); iterator.hasNext(); ) {
-					MicroService value = iterator.next();
-					microServiceToMsgQueueMap.get(value).add(b);
-				}
-//				for (MicroService ms : broadcastToMicroServiceQueueMap.get(b.getClass())) { // check if need to be synchronized
-//					//synchronized (microServiceToMsgQueueMap.get(ms)) {
-//						microServiceToMsgQueueMap.get(ms).add(b);
-				//}
-				//}
-			}
-		}
+		if (broadcastToMicroServiceQueueMap.get(b.getClass()) != null) // there's micro-service subscribed to b type of broadcast
+			for (MicroService microService : broadcastToMicroServiceQueueMap.get(b.getClass())) // adding broadcast to each micro-service in the concurrent list
+				microServiceToMsgQueueMap.get(microService).add(b);
 	}
-
-	
-//	//@Override
-//	public <T> Future<T> sendEvent2(Event<T> e) {
-//		// check if theres a subscriber for this type
-//		// if there is, take the next subscriber by robinhood
-//		// 		add the event to that subscriber messgage queue
-//		// 		put back the subs to the robinhood
-//		//		return the future
-//		// else return null
-//		Future<T> future = null;
-//		ConcurrentLinkedQueue<MicroService> subscribersQueue = eventToMicroServicesQueueMap.get(e.getClass()); // queue of the micro-services that subscribed to this type of event
-//		if (subscribersQueue != null) { // there's such queue
-//			synchronized (eventToMicroServicesQueueMap.get(e.getClass())) { // first thread to get to round-robin queue, no other thread will mess with that queue until he finish
-//				MicroService ms = subscribersQueue.poll(); // get next micro-service in the queue (by round-robin)
-//				if (ms != null) { // there's a subscriber in the queue @INV: subscribersQueue never empty
-//					boolean successAdd = microServiceToMsgQueueMap.get(ms).offer(e); // add that event the proper micro-service message queue
-//					if (!successAdd){ // DEBUG PURPOSES
-//						System.out.println("WTF WHY FAILED TO ADD EVENT: " + e + " TO MICROSERVICE:" + ms);
-//					}
-//					subscribersQueue.add(ms); // add back the micro-service to the subscriber-queue (by round-robin)
-//					future = new Future<T>();
-//					eventToFutureMap.put(e, future);
-//				}
-//			}
-//		}
-//		return future;
-//	}
 
 	@Override
 	public <T> Future<T> sendEvent(Event<T> e) {
-		// check if theres a subscriber for this type
-		// if there is, take the next subscriber by robinhood
-		// 		add the event to that subscriber messgage queue
-		// 		put back the subs to the robinhood
-		//		return the future
-		// else return null
 		Future<T> future = null;
-		MicroService nextToReceiveEvent = null;
-		ConcurrentLinkedQueue<MicroService> subscribersQueue = eventToMicroServicesQueueMap.get(e.getClass()); // queue of the micro-services that subscribed to this type of event
-		if (subscribersQueue != null) { // there's such queue - if no means no one is subscribe to this type of event
-			synchronized (eventToMicroServicesQueueMap.get(e.getClass())) { // first thread to get to round-robin queue, no other thread will mess with that queue until he finish
-				nextToReceiveEvent = subscribersQueue.poll(); // get next micro-service in the queue (by round-robin)
-				if (nextToReceiveEvent == null) // queue is empty
-					return null; // no micro-service to receive the event, (the sender will handle properly)
-				subscribersQueue.add(nextToReceiveEvent); // add back the micro-service to the subscriber-queue (by round-robin)
-			}
-
-			if (microServiceToMsgQueueMap.get(nextToReceiveEvent) != null){ // ad
-				future = new Future<T>();
-				eventToFutureMap.put(e, future);
-				if (!microServiceToMsgQueueMap.get(nextToReceiveEvent).offer(e))
-					System.out.println("WTF WHY FAILED TO ADD EVENT: " + e + " TO MICROSERVICE:" + nextToReceiveEvent.getName()); // DEBUG PURPOSES
-			}
-			else // DEBUG PURPOSES
-				System.out.println("WTF WHY FAILED TO GET THE MESSAGE QUEUE OF " + nextToReceiveEvent.getName());
+		MicroService nextToReceiveEventMicroService;
+		if (eventToMicroServicesQueueMap.get(e.getClass()) != null) { // there's such queue - if no means no one is subscribe to this type of event
+			nextToReceiveEventMicroService = getNextEventSubscriber(e);
+			future = addEventToMicroService(nextToReceiveEventMicroService,e);
 		}
 		return future;
 	}
 
+	// get next subscriber of the event type (by round robin)
+	private <T> MicroService getNextEventSubscriber (Event<T> e) {
+		synchronized (eventToMicroServicesQueueMap.get(e.getClass())) { // first thread to get to round-robin queue, no other thread will mess with that queue until he finish
+			ConcurrentLinkedQueue<MicroService> subscribersQueue;
+			subscribersQueue = eventToMicroServicesQueueMap.get(e.getClass());
+			MicroService nextToReceiveEvent = subscribersQueue.poll(); // get next micro-service in the queue (by round-robin)
+			if (nextToReceiveEvent != null)
+				subscribersQueue.add(nextToReceiveEvent); // add back the micro-service to the subscriber-queue (by round-robin)
+			return nextToReceiveEvent;
+		}
+	}
+
+	// add the event to nexToToReceive event's queue
+	private <T> Future<T> addEventToMicroService (MicroService nextToReceiveEvent, Event<T> e) {
+		Future<T> future = null;
+		if ((nextToReceiveEvent != null) && (microServiceToMsgQueueMap.get(nextToReceiveEvent) != null)) { // ad
+			future = new Future<>();
+			eventToFutureMap.put(e, future);
+			microServiceToMsgQueueMap.get(nextToReceiveEvent).offer(e);
+		}
+		return future;
+	}
 
 	@Override
 	public void register(MicroService m) {
-		// check if already registered
-		microServiceToEvent.putIfAbsent(m,new Vector<>());
-		microServiceToBroadcast.putIfAbsent(m,new Vector<>());
+		microServiceToEvent.putIfAbsent(m,new ConcurrentLinkedQueue<>());
+		microServiceToBroadcast.putIfAbsent(m,new ConcurrentLinkedQueue<>());
 		microServiceToMsgQueueMap.putIfAbsent(m,new LinkedBlockingQueue<>());
 	}
 
 	@Override
-	public void unregister(MicroService m) { // TO DO: might need to be synchronized
-		// delete m from round robin queue and from broadcast map
-		// if after unregister the queue of subscribers for some type of event is empty --> delete the queue
-
-		Vector<Class<? extends Event>> eventTypes  = microServiceToEvent.get(m);
-		Vector<Class<? extends Broadcast>> broadcastsTypes = microServiceToBroadcast.get(m);
-		for (Class<? extends Event> type: eventTypes )
-			synchronized (eventToMicroServicesQueueMap.get(type)){
-				eventToMicroServicesQueueMap.get(type).remove(m);
-			}
-		for (Class<? extends Broadcast> type: broadcastsTypes)
-			synchronized (broadcastToMicroServiceQueueMap.get(type)) {
-				broadcastToMicroServiceQueueMap.get(type).remove(m);
-			}
-		microServiceToMsgQueueMap.remove(m); // what to do if m has messages in its queue and it unregistered itself?
+	public void unregister(MicroService m) {
+		for (Class<? extends Event> eventType: microServiceToEvent.get(m) ) // each event type that m is subscribed to
+			eventToMicroServicesQueueMap.get(eventType).remove(m); // remove m from the type's micro-services queue
+		for (Class<? extends Broadcast> broadcastType: microServiceToBroadcast.get(m)) // each broadcast type that m is subscribed to
+			broadcastToMicroServiceQueueMap.get(broadcastType).remove(m); // remove m from the type's micro-services queue
+		microServiceToMsgQueueMap.remove(m);
 		microServiceToBroadcast.remove(m);
 		microServiceToEvent.remove(m);
 	}
 
 	@Override
 	public Message awaitMessage(MicroService m) throws InterruptedException {
-		//synchronized (microServiceToMsgQueueMap.get(m)) {
-			return microServiceToMsgQueueMap.get(m).take(); // blocking method until new message is received
-		//}
+		return microServiceToMsgQueueMap.get(m).take(); // blocking method until new message is received
 	}
 }
